0a63b9c830abad543e05033f76c2feef312de39e
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2025-04-01 13:48:58 -0500

    Update 01_bloch_align.sh

--- 01_bloch_align.sh
+++ 01_bloch_align.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --chdir=.
 #SBATCH --job-name=align
-#SBATCH --partition nocona
+#SBATCH --partition=nocona
 #SBATCH --nodes=1 --ntasks=8
 #SBATCH --mem-per-cpu=8G
 #SBATCH --array=1-30
