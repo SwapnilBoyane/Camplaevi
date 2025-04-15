50efc021193c5be45e8ed1c47bb3751ce4288750
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2025-04-15 15:37:02 -0500

    Update Admiture.sh

--- Admiture.sh
+++ Admiture.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --chdir=./
 #SBATCH --job-name=admixture
-#SBATCH --partition nocona
+#SBATCH --partition=nocona
 #SBATCH --nodes=1 --ntasks=4
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=16G
