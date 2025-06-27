a391544366b5c1dc2e1300d450bfe9afff3bb6c0
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2025-06-27 15:11:21 -0500

    Update 04_filter_camplaevi.sh

--- 04_filter_camplaevi.sh
+++ 04_filter_camplaevi.sh
@@ -23,11 +23,11 @@ vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep keeplist.txt  --max-
 
 # run vcftools with SNP and invariant site output, 20% max missing data, no indels
 #for phylogeny and observed heterozygosity remove outgroup
-vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep keeplist.txt --max-missing 0.8 --max-alleles 2  --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/08_OH/${region_array}
+vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep keeplist.txt --max-missing 0.8 --max-alleles 2  --max-maf 0.49 --remove-indels --recode --recode-INFO-all --out ${workdir}/08_phylo/${region_array}
 
 # run bcftools to simplify the vcftools output for observed heterozygosity analysis
 #bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n ' ${workdir}/08_OH/${region_array}.recode.vcf > ${workdir}/08_OH/${region_array}.simple.vcf
 
 # compress files with bgzip and index with tabix
-~/anaconda3/bin/bgzip ${workdir}/05_filtered_vcf/${region_array}.recode.vcf
-~/anaconda3/bin/tabix ${workdir}/05_filtered_vcf/${region_array}.recode.vcf.gz
+~/anaconda3/bin/bgzip ${workdir}/08_phylo/${region_array}.recode.vcf
+~/anaconda3/bin/tabix ${workdir}/08_phylo/${region_array}.recode.vcf.gz
